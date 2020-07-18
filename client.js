ca1f0d65fa40585a6fd79a1316ef486f49abc485
Pedro Gonzalez <pgonzalez@post.com>
2020-07-18 02:35:49 -0500

    Agregar petición al servidor y cambiar meow por miau

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ console.log('Meow!');
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const API_URL = 'http://localhost:5000/miaus'
 
 loadingElement.style.display = 'none';
 
@@ -11,13 +12,22 @@ form.addEventListener('submit', (e) => {
   const name = formData.get('name');
   const content = formData.get('content');
 
-  const meow = {
+  const miau = {
     name,
     content
   }
 
   form.style.display = 'none';
   loadingElement.style.display = '';
-  console.log(meow);
+
+  fetch(API_URL, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(miau)
+  }).then(response => {
+      console.log(response.json());
+    });
 
 });
